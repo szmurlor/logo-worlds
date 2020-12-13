f01598d4423750ddc9909ff6ceca92bc60828699
Robert Szmurlo <robert.szmurlo@ee.pw.edu.pl>
2020-12-13 17:24:15 +0100

    Prepared for hosting web-client bundle

--- service/instance/flask_app.py
+++ service/instance/flask_app.py
@@ -1,7 +1,7 @@
 # instance/flask_app.py
 
 # third-party imports
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
@@ -13,7 +13,7 @@ from misc.service_logger import serviceLogger as logger
 from core.db_connection import DataBase
 
 # flask application initialization
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../web-client-build", static_url_path="")
 
 # cross origin resource sharing
 CORS(app)
@@ -22,13 +22,18 @@ CORS(app)
 try:
     pass
     #connection = DataBase().get()
-    #connection.close()
+    # connection.close()
 except Exception as ex:
     logger.error(cn.DB_UNAVAILABLE, exc_info=True)
     print("Database unavailable")
     connection = None
 
-api = Api(title="Logo Worlds API", prefix="/worlds/api/v1", description="Logo Worlds Simulator (c) Robert Szmurlo 2020")
+api = Api(title="Logo Worlds API", prefix="/worlds/api/v1",
+          description="Logo Worlds Simulator (c) Robert Szmurlo 2020\n\n"
+          "Przejdź do <a href='index.html' target='_blank'>Logo World Game</a> aby ZAGRAĆ w grę we własnym świecie :-)\n\n"
+          "<img src='world_demo.png' height='200px' alt='Logo World Demo' />\n\n"
+          "Instrukcja może być znaleziona tutaj: \n\n"
+)
 api.init_app(app)
 
 # Flask app configurations
@@ -43,7 +48,6 @@ if config["database"].as_bool("bootstrapdb"):
     from core.bootstrapdb import bootstrapdb
     bootstrapdb()
 
-
 from routes.worlds import worlds
 
 if config["database"].as_bool("dumpdb"):
@@ -57,3 +61,12 @@ if config["database"].as_bool("dumpdb"):
         for table_name in inspector.get_table_names(schema=schema):
             for column in inspector.get_columns(table_name, schema=schema):
                 print("Column: %s" % column)
+
+# @app.route('/game/<path:path>')
+# def serve_page(path):
+#     print(path)
+#     print(app.root_path)
+#     import os
+#     for f in os.walk(os.path.join(app.root_path, '../web-client-build')):
+#         print(f)
+#     return send_from_directory(os.path.join(app.root_path, '../web-client-build'), path)
